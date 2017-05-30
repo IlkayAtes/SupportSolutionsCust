380907d9d88532de024e14cd8da4a3206e3ee4cb
IlkayAtes <ilkay.ates.de@gmail.com>
2017-05-30 15:11:02 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("SupportSolutionsCustsupportsolutionscust.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("SupportSolutionsCustsupportsolutionscust.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
